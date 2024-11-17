03f08359fb76de04a470b4d8391ac820002fe8a1
Michael schrijvers <michael.10330@hotmail.com>
2024-11-17 20:04:35 +0100

    basic Js and html added

---
+++ core.js
@@ -0,0 +1,10 @@
+//render the list based on what is in local storage
+//ability to place something into local storage based on key value pairs
+//what value pairs?
+//be able to delete something from the list
+//completely clear list
+
+const outputs = document.getElementById("todolist");
+const addButton = document.getElementById("addbutton");
+const input = document.getElementById("textinput");
+
