15d59cb6bbf46e80a3a6b46fedb9adc459cbfc86
Michael schrijvers <michael.10330@hotmail.com>
2024-11-18 13:11:07 +0100

    added the code

--- core.js
+++ core.js
@@ -7,4 +7,69 @@
 const outputs = document.getElementById("todolist");
 const addButton = document.getElementById("addbutton");
 const input = document.getElementById("textinput");
+const clearButton = document.getElementById("clearbutton");
+let posts;
+function AddPost() {
+    console.log("clicked")
+    console.log(posts, ' posts na parse')
+    const post = {
+        toDoText: GetTodo()
+    }
+    posts.push(post);
+    console.log(posts);
+    localStorage.setItem("storedPosts", JSON.stringify(posts))
+    DrawList();
+    console.log(localStorage.getItem("storedPosts"), " posts na toevoeging")
 
+}
+
+function GetTodo() {
+    let todoData = document.getElementById("textinput").value;
+    console.log(todoData);
+    if (todoData) {
+        console.log('niet undefined');
+
+        input.value = '';
+        return todoData;
+    } else {
+        throw new Error('no input');
+    }
+}
+
+function DrawList() {
+    outputs.innerHTML = '';
+    for (i = 0; i < posts.length; i++) {
+
+        outputs.innerHTML += `
+        <fieldset role="group">
+            <li class = ""><p>${i + 1}. ${posts[i].toDoText}<p>
+             <button onclick = "DeletePost(${i})">delete</button>
+            </li>
+        </fieldset>
+        `
+    }
+}
+
+function InitializeLocalStorage() {
+    posts = JSON.parse(localStorage.getItem("storedPosts")) || [];
+    console.log(posts);
+}
+
+function ClearCache() {
+    console.log('cleared')
+    localStorage.clear();
+    InitializeLocalStorage();
+    DrawList();
+}
+
+function DeletePost(id){
+    posts.splice(id, 1);
+    localStorage.setItem("stored posts", JSON.stringify(posts));
+    DrawList();
+}
+//localStorage.clear();
+InitializeLocalStorage();
+DrawList();
+console.log(addButton);
+addButton.addEventListener("click", AddPost);
+clearButton.addEventListener("click", ClearCache);
